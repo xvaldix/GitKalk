c3fc1ab37fd3a16cb8ba7ba7d7bd81525199a718
Borowski Waldemar <Waldemar.Borowski@t-mobile.pl>
2023-02-13 18:47:09 +0100

    utworzylem class Actions zamiast funkcji

--- test23.01.py
+++ test23.01.py
@@ -1,36 +1,37 @@
-def add (var1, var2) -> int:
-    return var1 + var2
+class Actions:
+    def add (var1, var2) -> float:
+        return var1 + var2
 
-def div (var1, var2: int) -> float:
-    return var1 / var2
+    def div (var1, var2: float) -> float:
+        return var1 / var2
 
-def sub (var1, var2):
-    return var1 - var2
+    def sub (var1, var2):
+        return var1 - var2
 
-def mul (var1, var2):
-    return var1 * var2
+    def mul (var1, var2):
+        return var1 * var2
 
 while True:
     znak = input("Podaj znak działania < +, -, *, / >: ")
     if znak == "+":
         var1 = float(input("Podaj pierwsza liczbe: "))
         var2 = float(input("Podaj druga liczbe: "))
-        print(f"{add(var1, var2)}")
+        print(f"{Actions.add(var1, var2)}")
     elif znak == "-":
         var1 = float(input("Podaj pierwsza liczbe: "))
         var2 = float(input("Podaj druga liczbe: "))
-        print(f"{sub(var1, var2)}")
+        print(f"{Actions.sub(var1, var2)}")
     elif znak == "/":
         var1 = float(input("Podaj pierwsza liczbe: "))
         var2 = float(input("Podaj druga liczbe: "))
         while var2 == 0:
             var2 = float(input("Podaj druga liczbe, nie może to być zero: "))
         else:
-            print(f"{div(var1, var2)}")
+            print(f"{Actions.div(var1, var2)}")
     elif znak == "*":
         var1 = float(input("Podaj pierwsza liczbe: "))
         var2 = float(input("Podaj druga liczbe: "))
-        print(f"{mul(var1, var2)}")
+        print(f"{Actions.mul(var1, var2)}")
     else:
         print(f"Wprowadzono błędny znak.")
     
